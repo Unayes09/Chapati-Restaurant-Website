bb692a36216fd2f80b3c61881f362b2294642b6f
Unayes09 <unayeskhan.0808@gmail.com>
2026-03-14 17:34:05 +0600

    email config upgraded

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -4,10 +4,16 @@ const sendEmail = async (to, subject, text, html) => {
   try {
     const transporter = nodemailer.createTransport({
       service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
       },
+      tls: {
+        rejectUnauthorized: false // This helps bypass some cloud security blocks
+      }
     });
 
     const mailOptions = {
